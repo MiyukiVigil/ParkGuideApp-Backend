219f84e7aaeaf6f052e00cb2dd3d69f1c43fcacf
MiyukiVigil <miyukivigil@gmail.com>
2026-04-21 14:53:05 +0800

    Updated the crazy stuff

--- park_guide/well_known.py
+++ park_guide/well_known.py
@@ -1,11 +1,22 @@
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+
+
+def _assetlinks_file_path():
+    return settings.BASE_DIR / '.well-known' / 'assetlinks.json'
 
 
 def assetlinks_json(request):
+    assetlinks_path = _assetlinks_file_path()
+    if assetlinks_path.exists():
+        return HttpResponse(assetlinks_path.read_text(encoding='utf-8'), content_type='application/json')
+
     payload = [
         {
-            "relation": ["delegate_permission/common.get_login_creds"],
+            "relation": [
+                "delegate_permission/common.handle_all_urls",
+                "delegate_permission/common.get_login_creds",
+            ],
             "target": {
                 "namespace": "android_app",
                 "package_name": settings.PASSKEY_ANDROID_PACKAGE_NAME,
